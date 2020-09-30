0cce393d4ab31618993f2ac9a5867cf68128b74a
Cristian Guaman <cristian.guaman@icloud.com>
2020-09-30 02:28:18 -0500

    added GraphQL

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_iRxp8DLtY",
     "aws_user_pools_web_client_id": "285hqe5100q5gg9f75p6raeja6",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://vvrmr6k6nffr7hr5vfwnkcvvve.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-fh5ebizxjngzzlfgx5c3nw5c7y"
 };
 
 
